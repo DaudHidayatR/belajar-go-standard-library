54d98dc37cb15c92c128e832721a2135b0302940
Daud Hidayat Ramadahan <daud28ramadhan@gmail.com>
2025-01-05 13:10:11 +0700

    feat: package Math
    Signed-off-by: Daud Hidayat Ramadahan <daud28ramadhan@gmail.com>

---
+++ math.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Ceil(2.7))
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(math.Sqrt(16))
+	fmt.Println(math.Round(2.7))
+	fmt.Println(math.Max(2, 7))
+	fmt.Println(math.Min(2, 7))
+
+}
